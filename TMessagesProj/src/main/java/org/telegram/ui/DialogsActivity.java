15a4836fe49701b6978862dccab04d52c8ad7a8f
Next Alone <12210746+NextAlone@users.noreply.github.com>
2023-05-17 23:36:55 +0800

    fix: do not unarchive by swipe
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -2234,12 +2234,6 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
 
         @Override
         public float getSwipeThreshold(RecyclerView.ViewHolder viewHolder) {
-            if (viewHolder != null && viewHolder.itemView instanceof DialogCell) {
-                if (((DialogCell) viewHolder.itemView).getCurrentDialogFolderId() == 0
-                    && ConfigManager.getBooleanOrFalse(Defines.doNotUnarchiveBySwipe)) {
-                    return 1.0f;
-                }
-            }
             return 0.45f;
         }
 
